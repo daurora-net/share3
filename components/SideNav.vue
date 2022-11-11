c517f30d2516864a4233b2f2e790f1619f96682c
daurora-net <daurora.net@gmail.com>
2022-11-12 02:52:15 +0900

    side5

--- components/SideNav.vue
+++ components/SideNav.vue
@@ -3,16 +3,16 @@
     <img class="side-nav_logo" src="../assets/img/logo.png" alt="">
     <NuxtLink to="/"><img class="side-nav_icon" src="../assets/img/home.png" alt="">ホーム</NuxtLink>
     <NuxtLink to="/logout"><img class="side-nav_icon" src="../assets/img/logout.png" alt="">ログアウト</NuxtLink>
-    <div>
+    <div class="share_box">
       <!-- <input class="share_box" type="text" v-model="text">
       <p>{{ text | upperCase }}</p> -->
       <!-- <input type="text" name="addName" v-model="content" placeholder="タスクを入力してください">
       <button class="button button--green" @click="insert">追加</button> -->
-
-          <form>
-            <input type="text" class="share_box" v-method="content" />
-            <button class="button side-form_btn" @click="insert">シェア</button>
-          </form>
+      <p class="share_box_ttl">シェア</p>
+      <form>
+        <input type="text" class="share_box_txt" v-method="content" />
+        <button class="button side-form_btn" @click="insert">シェアする</button>
+      </form>
     </div>
   </div>
 </template>
