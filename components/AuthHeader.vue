a2849381a098371ff78e367f7de1386778508e1d
daurora-net <daurora.net@gmail.com>
2022-11-12 01:11:27 +0900

    register_logo

--- components/AuthHeader.vue
+++ components/AuthHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header_nav">
-    <img class="logo" src="../assets/img/logo.png" alt="">
+    <img class="logo_img" src="../assets/img/logo.png" alt="">
     <NuxtLink to="/register">新規登録</NuxtLink>
     <NuxtLink to="/login">ログイン</NuxtLink>
   </div>
